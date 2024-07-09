54e736c2cf1a550471bd84554e326a3beba5258c
LAL102LAL <bobychs66@gmail.com>
2024-07-09 21:02:46 +0530

    Add initial HTML file

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('joinButton').addEventListener('click', function() {
+   
+    window.location.href = 'https://t.me/+Sbf3-FB-NGJhNzVl'; // replace with your Telegram channel link
+});
